c4984f0af74c769b683fa2ccae059b674ab8ea00
Elizabeth Bartholomew <lizzy.bartholomew@gmail.com>
2023-06-13 10:17:43 -0600

    Summary

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('form button');
+button.addEventListener('click', () =>{
+    const element = document.querySelector('form button');
+    element.style.backgroundColor = "hsl(25, 97%, 53%)";
+});
